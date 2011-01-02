8e25bb1ee233b3b456e5d07a24538a941968667a
maelstrom <heap.tim@gmail.com>
2011-01-02 19:23:51 +1100

    Started working on some improved signal handling
    Classes can register any signals they emit by suppling a signal name and
    a callback, which should handle the signal and pass it to the JavaScript
    function.

--- src/ngtk_button.h
+++ src/ngtk_button.h
@@ -8,6 +8,7 @@ namespace ngtk {
 class Button : public Widget {
 public:
   static void       SetPrototypeMethods (v8::Handle<v8::FunctionTemplate> constructor_template);
+  static void       RegisterCallbacks   (std::vector<SignalCallback> *callbacks);
   static void       Initialize          (v8::Handle<v8::Object> target);
   static Button*    New                 (void); // public constructor
   static bool       HasInstance         (v8::Handle<v8::Value> val);
@@ -16,9 +17,10 @@ private:
   static v8::Persistent<v8::FunctionTemplate> constructor_template;
 
   static v8::Handle<v8::Value> New      (const v8::Arguments &args);
-  // gtk_button_*_label()
+
   static v8::Handle<v8::Value> SetLabel (const v8::Arguments &args);
   static v8::Handle<v8::Value> GetLabel (const v8::Arguments &args);
+
   static v8::Handle<v8::Value> SetImage (const v8::Arguments &args);
   static v8::Handle<v8::Value> SetImagePosition (const v8::Arguments &args);
 
--- src/ngtk_widget.h
+++ src/ngtk_widget.h
@@ -2,13 +2,26 @@
 #define NGTK_WIDGET_H_
 
 #include <node_object_wrap.h> // node::ObjectWrap
+#include "v8.h"
 #include "ngtk.h"
+#include <vector>
 
 namespace ngtk {
+
+struct SignalCallback {
+  const char *name;
+  GCallback callback;
+  
+  SignalCallback (const char *name, GCallback callback) {
+    this->name = name;
+    this->callback = callback;
+  }
+};
 
 class Widget : public node::ObjectWrap {
 public:
   static void SetPrototypeMethods (v8::Handle<v8::FunctionTemplate> constructor_template);
+  static void RegisterCallbacks   (std::vector<SignalCallback> *callbacks);
 
   // For getting the underlying GtkWidget
   static inline GtkWidget* Data (v8::Handle<v8::Object> obj) {
@@ -18,17 +31,30 @@ public:
 
   GtkWidget *widget_;
 
+  static std::vector<SignalCallback> *callbacks;
+
+  // Event handlers.
+  static void     SignalBare   (GtkWidget *widget, gpointer callback_ptr);
+  static gboolean SignalBoolean(GtkWidget *widget, gpointer callback_ptr);
+  static gboolean SignalFocus  (GtkWidget *widget, GtkDirectionType direction, gpointer callback_ptr);
+
 private:
   // Add signal handler.
-  static v8::Handle<v8::Value> On      (const v8::Arguments &args);
+  static v8::Handle<v8::Value> On         (const v8::Arguments &args);
+  // remove signal handler.
+  static v8::Handle<v8::Value> Disconnect (const v8::Arguments &args);
+
   // For showing the widget.
-  static v8::Handle<v8::Value> Show    (const v8::Arguments &args);
+  static v8::Handle<v8::Value> Show       (const v8::Arguments &args);
   // For destroying widgets
-  static v8::Handle<v8::Value> Destroy (const v8::Arguments &args);
-  // Event handler.
-  // TODO: Improve how we handle signals. Signals have variable arguments...
-  static void onSignal (GtkWidget *widget, gpointer callback_ptr);
+  static v8::Handle<v8::Value> Destroy    (const v8::Arguments &args);
+
+  // For checking focus
+  static v8::Handle<v8::Value> IsFocus    (const v8::Arguments &args);
+  // For grabbing focus
+  static v8::Handle<v8::Value> GrabFocus  (const v8::Arguments &args);
 };
+
 
 } // namespace ngtk
 
